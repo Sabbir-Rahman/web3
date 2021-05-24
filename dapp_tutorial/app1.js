50efc7f7272d2ec935db6127ae80c0a755d731b9
Sabbir-Rahman <sabbirrahman-42@iut-dhaka.edu>
2021-05-24 16:50:05 +0600

    first tutorual

---
+++ dapp_tutorial/app1.js
@@ -0,0 +1,21 @@
+var Web3 = require('web3')
+//show the web3 basic function
+// console.log(Web3)
+
+//project url taken from infura this same thing can be done by ganache
+var url = 'https://mainnet.infura.io/v3/3dbcdfed652e4478ba9bba18c482755d'
+var web3_url = new Web3(url)
+//showing more function of that url project
+// console.log(web3_url)
+
+var address = '0x00000000219ab540356cBB839Cbe05303d7705Fa'
+
+//check the balance of the upper account
+web3_url.eth.getBalance(address, (err, bal) => { balance = bal})
+
+//below command work in node terminal only
+
+// balance
+// web3_url.utils.fromWei(balance, 'ether')
+
+// web3_url.eth.accounts.create()
